53ce9c6eae2bfb7f10d2fd18897f0e5d8e961f16
liufei <LiuFei@121>
2022-06-13 15:59:28 +0800

    测试

---
+++ src/algorithm/huiwen/Palindrome.java
@@ -0,0 +1,9 @@
+package algorithm.huiwen;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        int num = 101;
+        int a =num%100;
+        System.out.println(a);
+    }
+}
